2e40927ef8febbcf69a061b66be4f5fc351653d4
Danikileitor <focusyi@hotmail.com>
2023-02-10 09:47:28 +0100

    Contador

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.contador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public TextView lbContador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lbContador = findViewById(R.id.lbContador);
+    }
+
+    public void incrementar(View view){
+        int contador = Integer.parseInt(String.valueOf(lbContador.getText()));
+        contador++;
+        lbContador.setText(String.valueOf(contador));
     }
 }
